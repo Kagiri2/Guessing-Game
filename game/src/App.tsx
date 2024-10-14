a14ac27164961e0988506925c675623e7f726a5a
Derek Lee <37049496+Kagiri2@users.noreply.github.com>
2024-10-14 15:44:46 -0400

    ..

--- game/src/App.tsx
+++ game/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import Game from './components/Game';
+import { Game, Home } from './Components';
+
 
 const App = () => {
   return (
     <div className="bg-gray-100 h-screen flex items-center justify-center flex-col"> {/* Added flex-col to stack items vertically */}
       <h1 className="text-2xl font-bold text-center mb-4">Guessing Game</h1>
       <p className="text-lg text-center mb-4">Welcome to the Guessing Game! Try to guess the correct answer.</p> {/* Added text here */}
-      <Game />
+      <Home />
     </div>
   );
 };
---
+++ game/src/Components.tsx
@@ -0,0 +1,2 @@
+export { default as Game } from './components/Game';
+export { default as Home } from './pages/Home';
